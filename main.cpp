d8449f64a2716da55b6c5426cebd79e47ffae999
tt <2902594673@qq.com>
2021-10-24 14:56:33 +0800

    实现了sort函数

--- main.cpp
+++ main.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 void sort(int arr[],int n)
 {
-
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				int temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
 }
 int main()
 {
 	int arr[]={1,3,2,4,5,7};
 	int len=6;
 	sort(arr,len);
+	for(int i=0;i<len;i++) printf("%d ",arr[i]);
+	printf("\n");
 }
